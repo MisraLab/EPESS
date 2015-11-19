9b68fc4ae6383b2b10fa8ed0b4a9d3f5ba6b1bec
Francois Fagan <ff2316@columbia.edu>
2015-11-19 11:52:59 -0500

    Corrected the basic T simulation code so that it now works well. It can simulate a T using the 2nd kind of slice sampling algorithm (like in the ESS paper). It can be confirmed to be correct when compared to the exact T distribution. The code can also plot potential slices and I have added png's of plots of potential slices.

--- MATLAB code/T/T_basic_simulation.m
+++ MATLAB code/T/T_basic_simulation.m
@@ -1,41 +1,91 @@
+% This code runs the student's t "spiral slice sampling". It has the
+% following sections:
+%
+% 0. Set parameters and hyperparamters
+%
+% 1. Run algorithm
+%
+% 2. Plot potential slices
+%
+% 3. Plot distributions
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 0. Set parameters and hyperparamters
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% hyperparamters
+A = 5;
+B = 2;
+lambda = 1;
+dim = 2;
 
 % Algorithm parameters
 n_samples = 100000;
-t = zeros(1,n_samples);
+n_bins = 1000;
+t = zeros(n_samples,dim);
 
-% hyperparamters
-A = 1;
-B = 1;
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 1. Run algorithm
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Initialize
-f = randn;
-phi = 0.5;
-%t = gaminv(phi,A,B) * f;
+f = mvnrnd(zeros(1,dim),eye(dim));
+phi = rand;
 
 for sample_index = 1:n_samples
     
-    psi = rand;
-    s = gaminv(psi,A,B);
+    % v ~ normal (0,Sigma)
+    v = mvnrnd(zeros(1,dim),eye(dim));
+
+    % u ~ unif (0,1)
+
+    % eta ~ unif (0,2*pi)
+    eta = 2*pi*rand;
+
+    % z ~ unif ( (- phi) / cos(eta) ,  (1 - phi) / cos(eta) )
+    z = (rand - phi) / cos(eta) ;
     
-    theta = 2*pi*rand;
-    v = randn;
-    v_0 = f*sin(theta)+v*cos(theta);
-    v_1 = f*cos(theta)-v*sin(theta);
+    % New variables
+    f = f*cos(sin(eta)*z) +  v*sin(sin(eta)*z);%randn;%
+    phi = z*cos(eta) + phi;%rand;%
     
-%     % v ~ normal (0,Sigma)
-%     v = randn;
-% 
-%     % u ~ unif (0,1)
-% 
-%     % eta ~ unif (0,2*pi)
-%     eta = 2*pi*rand;
-% 
-%     % z ~ unif ( (- phi) / cos(eta) ,  (1 - phi) / cos(eta) )
-%     z = (rand - phi) / cos(eta) ;
-%     
-%     % New variables
-%     f = f*cos(sin(eta)*z) +  v*sin(sin(eta)*z);
-%     phi = z*cos(eta) + phi;
-    
-    t(sample_index) = gaminv(phi,A,B) * f;
+    t(sample_index,:) = f / sqrt(gaminv(phi,A,1/B));
 end
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2. Plot potential slices
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% step = 0.00001;
+% tt = t(length(t),:);
+% v = mvnrnd(zeros(1,dim),eye(dim));
+% figure
+% hold on
+% for i =1:200
+%     eta = 2*pi*rand;
+%     zz = ((step:step:(1-step))-phi) / cos(eta) ;
+%     t_arc = (f'*cos(sin(eta)*zz) +  v'*sin(sin(eta)*zz)) ./ repmat(sqrt(gaminv(zz*cos(eta) + phi,A,1/B)),2,1);
+%     plot(t_arc(1,:),t_arc(2,:),'-','Color',[rand,rand,rand])
+% end
+% plot(tt(1),tt(2),'x', 'markersize', 10,'Color','black','linewidth',3)
+% hold off
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 3. Plot distributions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Plot hist for 1-D, make sure that it is giving a T
+hist(t,n_bins)
+
+% Compare hist to actual T
+h = findobj(gca,'Type','patch');
+set(h,'FaceColor','r','EdgeColor','w','facealpha',0.5,'edgecolor','none')
+hold on
+hist(trnd(2*A,1,n_samples)*sqrt(B/(A*lambda)),n_bins)
+h1 = findobj(gca,'Type','patch');
+set(h1,'facealpha',0.5,'edgecolor','none');
+hold off
