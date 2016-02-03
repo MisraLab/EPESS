6f9763ccafa6fc246ac5d003f05f4d2c2a54d24a
Francois Fagan <ff2316@columbia.edu>
2016-02-03 09:26:59 -0500

    Changed empiricalKL so that takes care of repeated entries
    Now you don’t have to make sure that exact_samples and
    approximate_samples do not share common entries.

--- MATLAB_code/empiricalKLDivergence.m
+++ MATLAB_code/empiricalKLDivergence.m
@@ -4,13 +4,17 @@ function [ empirical_KL ] = empiricalKLDivergence( exact_samples, approximate_sa
 %are approximately from the true distribution.
 % Uses K=1 in KNN.
 
+    % Find the unique samples (i.e. no repetitions), otherwise there are issues with zero distances
+    unique_approximate_samples = setdiff(approximate_samples, exact_samples,'rows');
+    unique_exact_samples = setdiff(exact_samples, unique_approximate_samples,'rows');
+
     % Compute the nearest neighbour distances
-    [~, approximate_samples_distances] = knnsearch(approximate_samples, exact_samples); % Closest approximate sample to each exact sample
-    [~, exact_samples_distances] = knnsearch(exact_samples, exact_samples,'K',2); % Closest two exact samples to each exact sample (this includes the exact sample itself)
+    [~, approximate_samples_distances] = knnsearch(unique_approximate_samples, unique_exact_samples); % Closest approximate sample to each exact sample
+    [~, exact_samples_distances] = knnsearch(unique_exact_samples, unique_exact_samples,'K',2); % Closest two exact samples to each exact sample (so as to exclude the sample that the distance is measured from)
     exact_samples_distances = exact_samples_distances(:,2); % Only take the exact sample that is not equal to the sample from which the distances are measured
     
     % Compute empirical KL
-    empirical_KL = log(size(approximate_samples,1)) - log(size(exact_samples,1)-1)...
+    empirical_KL = log(size(unique_approximate_samples,1)) - log(size(unique_exact_samples,1)-1)...
         + 1 / length(exact_samples_distances) * sum(log(approximate_samples_distances) - log(exact_samples_distances));
 
 end
