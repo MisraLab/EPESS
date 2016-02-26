5f29d8bdc8647fd4d542d49a553b6a095ba3532e
Francois Fagan <ff2316@columbia.edu>
2016-02-26 18:14:52 -0500

    wrapper working well - thanks Lichi!

--- MATLAB_code/ep_probit/probit_sampling_wrapper.m
+++ MATLAB_code/ep_probit/probit_sampling_wrapper.m
@@ -2,42 +2,40 @@
 
 %% Parameters
 
-number_samples = 4000;
+number_samples = 400;
 number_chains = 1;
-number_examples = 400;
+number_examples = 1;
 frac_burnin = 0.1;
 rng(1)
 tic
 %% Load data
+dataset = 'musk';% bc, iono , pima , sonar , musk
 
-% Breast cander data
-load('data_bc.mat');
+% Read data and EP values
+load(['data_',dataset,'.mat']);
+load([dataset,'_EP_mean.mat']); %load('bc_EP_mean.mat');    %csvread('bc_EP_mean');
+load([dataset,'_EP_covariance.mat']);%load('bc_EP_covariance.mat');%EP_cov = csvread('bc_EP_variance');
 
-data_file = data_bc;
-
-dimension = size(data_bc,2) - 1;
-number_outcomes = size(data_bc,1);
+dimension = size(data,2) - 1;
+number_outcomes = size(data,1);
 
 % Transform so the covariates are zero mean and standard deviation 0.1.
 % This will make the Identity prior covariance matrix uninformative.
 
-Y = data_bc(:,size(data_bc,2)); % Read in the Y data
+Y = data(:,size(data,2)); % Read in the Y data
 if ~isempty(find(Y==0))
     Y = 2*Y-1; % Make Y -1,+1 instead of 0,1 if not already -1,+1
 end
 
-X = data_bc(:,1:(size(data_bc,2)-1))'; % Read in the X data
+X = data(:,1:(size(data,2)-1))'; % Read in the X data
 X = X.*repmat(Y,1,dimension)'; % Make X take the sign of Y so that in future only need to consider X
 
-% Read in EP mean and covariance
-EP_mean = csvread('bc_EP_mean');
-EP_cov = csvread('bc_EP_variance');
-EP_chol = chol(EP_cov);
+EP_chol = chol(EP_covariance);
 %% Sample
 
 logLikelihood = @(beta)(sum( log(normcdf(X'*beta'))) - 0.5*beta*beta'/100); % Probit likelihood with prior having variance 100 on each variable.
 
-for algorithm_index = 4:8
+for algorithm_index = 8:8
     eff_vec = zeros(1,number_examples);
     number_fn_evaluations_vec = zeros(1,number_examples);
     for example_index = 1:number_examples
