8b363547bc56a3c4180f04b3e1a099baca59a5a2
lichili233 <assaultarmor@gmail.com>
2016-02-26 17:36:22 -0500

    Fixed update to probit_tests.R

--- MATLAB_code/ep_probit/probit_tests.R
+++ MATLAB_code/ep_probit/probit_tests.R
@@ -1,24 +1,36 @@
 # Experiment Datasets for Probit Regression
 
-rm(list = ls())
+# rm(list = ls())
 
 setwd("/Users/Leechy/Documents/Columbia_Docs/Project_Research/ep-ess/src/breast_cancer")
 # setwd("/Users/francoisfagan/EPESS/EPESS/MATLAB_code/ep_probit")
 # rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-dataset_choice = 2
+dataset_choice = 1
 # 1 for Breast Cancer, 2 for Skin Sample, 3 for Pima Indian Diabetes, 
 # 4 for Ionosphere Radar, 5 for Musk Molecule, 6 for Sonar
 
 strip_outliers = TRUE # Strip data outliers for options 5 and 6
 
-library(R.matlab)
+num_examples = 1 # Number of iterations to loop over
+hmc_switch = TRUE # TRUE for ON, FALSE for OFF
+ep_switch = TRUE
+export_mean_cov = FALSE
 
-data_expt_names = c("bc","skin","pima","iono","musk","sonar")
+# HMC parameters
+number_samples <- 200
+number_chains <- 1
+
+library(R.matlab)
+library("rstan")
+library("EPGLM")
+
 
 ########################################################################
 # Read and format data
+
+data_expt_names = c("bc","skin","pima","iono","musk","sonar")
 
 if (dataset_choice == 1) {
   
@@ -173,43 +185,87 @@ if (dataset_choice == 1) {
   
 }
 
-########################################################################
-# HMC samples
+#########################################################
+# HMC (Stan) and EP (EPGLM) Inference
+table_mean_neff = c()
+table_num_leapfrog = c()
+table_neff_per_eval = c()
 
-library("rstan")
-#library("mvtnorm")
+for (iter_example in 1:num_examples) {
+  
+  if (hmc_switch == TRUE) {
+    ########################################################################
+    # HMC samples
+    
+    input_data <- list(N=N, M=M, x=x, y=y)
+    fit_stan <- stan("hmc_bc.stan", data=input_data, iter=number_samples, chains=number_chains)
+    
+    print(fit_stan)
+    sim=extract(fit_stan)
+    HMC_means <- colMeans(sim[1]$beta)
+    HMC_variance <- var(sim[1]$beta)
+    
+    inter_result <- fit_stan@sim$samples
+    num_leapfrog <- attributes(inter_result[[1]])$sampler_params$n_leapfrog__
+    total_func_evals <- sum(num_leapfrog) # Total numbers of leap frogs
+    summary_table <- summary(fit_stan)$summary
+    N_sum <- nrow(summary_table)
+    M_sum <- ncol(summary_table)
+    num_eff <- summary_table[1:(N_sum-1), M_sum-1] # Vector of n_eff's for all parameters
+    neff_per_eval <- mean(num_eff)/sum(num_leapfrog)
+    
+    table_mean_neff = c(table_mean_neff, mean(num_eff))
+    table_num_leapfrog = c(table_num_leapfrog, total_func_evals)
+    table_neff_per_eval = c(table_neff_per_eval, neff_per_eval)
+    
+    if (export_mean_cov == TRUE) {
+      # export HMC mean and covariance
+      hmcmean_fname = paste(data_expt_names[dataset_choice], "_HMC_mean.mat", sep="")
+      hmccov_fname = paste(data_expt_names[dataset_choice], "_HMC_covariance.mat", sep="")
+      writeMat(hmcmean_fname, HMC_mean = HMC_means)
+      writeMat(hmccov_fname, HMC_covariance = HMC_variance) 
+    }
+  }
 
-number_samples <- 1000
-number_chains <- 1
+  if (ep_switch == TRUE) {
+    ########################################################################
+    # EP approximation
+    
+    EP_approx <- EPprobit(x,y,100) # 1 is the prior variance of each variable
+    EP_means <- EP_approx$m
+    EP_variance <- EP_approx$V
+    
+    if (export_mean_cov == TRUE) {
+      # export EP mean and covariance
+      epmean_fname = paste(data_expt_names[dataset_choice], "_EP_mean.mat", sep="")
+      epcov_fname = paste(data_expt_names[dataset_choice], "_EP_covariance.mat", sep="")
+      writeMat(epmean_fname, EP_mean = EP_means) 
+      writeMat(epcov_fname, EP_covariance = EP_variance)    
+    }    
+  }
 
-input_data <- list(N=N, M=M, x=x, y=y)
-fit_stan <- stan("hmc_bc.stan", data=input_data, iter=number_samples, chains=number_chains)
+  if (hmc_switch == TRUE && ep_switch == TRUE) {
+    ########################################################################
+    # Difference between HMC approximation and EP approximation
+    print(HMC_means - EP_means)    
+  }
 
-print(fit_stan)
-sim=extract(fit_stan)
-HMC_means <- colMeans(sim[1]$beta)
-HMC_variance <- var(sim[1]$beta)
+  # End of example iteration
+}
 
-# export HMC mean and covariance
-hmcmean_fname = paste(data_expt_names[dataset_choice], "_HMC_mean.mat", sep="")
-hmccov_fname = paste(data_expt_names[dataset_choice], "_HMC_covariance.mat", sep="")
-writeMat(hmcmean_fname, HMC_mean = HMC_means)
-writeMat(hmccov_fname, HMC_covariance = HMC_variance)
+# Display results
+if (hmc_switch == TRUE) {
+  print(paste("Today is", date()))
+  
+  print(paste("Dataset:", data_expt_names[dataset_choice]))
+  
+  print(paste("Number of Examples:", num_examples))
+  print(paste("Mean of [n_eff]:", mean(table_mean_neff)))
+  print(paste("Std.Dev. of [n_eff]:", sd(table_mean_neff)))
+  print(paste("Mean of [n_f_eval]:", mean(table_num_leapfrog)))
+  print(paste("Std.Dev of [n_f_eval]:", sd(table_num_leapfrog)))
+  print(paste("Mean of [n_eff/n_f_eval]:" , mean(table_neff_per_eval)))
+  print(paste("Std.Dev. of [n_eff/n_f_eval]:", sd(table_neff_per_eval)))
+  
+}
 
-########################################################################
-# EP approximation
-
-library("EPGLM")
-EP_approx <- EPprobit(x,y,1) # 1 is the prior variance of each variable
-EP_means <- EP_approx$m
-EP_variance <- EP_approx$V
-
-# export EP mean and covariance
-epmean_fname = paste(data_expt_names[dataset_choice], "_EP_mean.mat", sep="")
-epcov_fname = paste(data_expt_names[dataset_choice], "_EP_covariance.mat", sep="")
-writeMat(epmean_fname, EP_mean = EP_means) 
-writeMat(epcov_fname, EP_covariance = EP_variance)
-
-########################################################################
-# Difference between HMC approximation and EP approximation
-print(HMC_means - EP_means)
