e10b304fb761ea0f0f36b32756755b3f97bb52fe
Francois Fagan <ff2316@columbia.edu>
2016-02-27 21:15:18 -0500

    Added not slice

---
+++ MATLAB_code/epessSamplerNOTslice.m
@@ -0,0 +1,33 @@
+function [ samples, nu ,number_fn_evaluations ] = epessSamplerNOTslice( number_samples , dimension, number_chains, logLikelihood, EP_mean, EP_chol, initial_point ) %axis_interval
+%Outputs samples from epess
+
+    pseudoLogLikelihoodShifted = @(x)( logLikelihood(x+EP_mean) - logGaussPdfChol(x', zeros(dimension,1), EP_chol));
+
+    % Initialize
+    samples = zeros(number_samples , dimension, number_chains);
+    nu = zeros(number_samples , dimension, number_chains);
+    number_fn_evaluations = 1;
+        
+    for chain_index = 1:number_chains
+
+        % Initialize chain        
+        if (nargin < 7) || isempty(initial_point)
+            initial_point = 0;
+        end
+        samples(1,:,chain_index) = initial_point;%axis_interval*(2*rand(1,dimension)-1);
+        cur_log_like = pseudoLogLikelihoodShifted(initial_point);
+        cur_number_fn_evaluations = 0;
+
+        % Run MCMC
+        for sample_index = 2 : number_samples
+                     
+            [samples(sample_index,:,chain_index), cur_log_like , cur_number_fn_evaluations, nu(sample_index,:,chain_index)] = elliptical_Notslice( samples(sample_index-1,:,chain_index), EP_chol, pseudoLogLikelihoodShifted, cur_log_like);
+            number_fn_evaluations = number_fn_evaluations + 1 + (cur_number_fn_evaluations-1) / dimension;
+            
+            
+        end
+    end
+    samples = samples + repmat(EP_mean , number_samples, 1 , number_chains);
+    nu = nu + repmat(EP_mean , number_samples, 1 , number_chains);
+end
+
